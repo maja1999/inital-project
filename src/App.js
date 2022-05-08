625aa0d9c58112ef85d7bdd5a86cd6e6775197dd
maja1999 <majcezmajce1999@gmail.com>
2022-05-08 17:04:05 +0200

    feat: toggle button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [toggle, setToggle] = useState(true);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {toggle ? <img src={logo} className="App-logo" alt="logo" /> : null}
+        <button
+          onClick={() => {
+            setToggle(!toggle);
+          }}
+        >
+          button
+        </button>
       </header>
     </div>
   );
